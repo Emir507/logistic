e95e7432d563e3d119c094498df9ca49a3f50985
Emir <dzhetybaev19emir@gmail.com>
2020-10-17 01:35:06 +0600

    delivery form component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    
+    <h1>Заказать доставку</h1>
+    <FormComponent />
   </div>
 </template>
 
 <script>
+import FormComponent from './../components/FormComponent';
 export default {
+  components: { FormComponent }
 }
 </script>

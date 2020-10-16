339d97df7f495b175dcc3ac63708d13bdd4f4bac
Emir <dzhetybaev19emir@gmail.com>
2020-10-17 03:07:50 +0600

    ApplicationsComponent imported

--- src/views/Applications.vue
+++ src/views/Applications.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    
+    <ApplicationsComponent />
   </div>
 </template>
 <script>
+import ApplicationsComponent from '../components/ApplicationsComponent'
 export default {
-  
+  components: {
+    ApplicationsComponent
+  }
 }
 </script>
